77feab1800ab2e95592fd43812ad6a20933d0c01
Julianne Vela <75726146+julianne-vela@users.noreply.github.com>
2021-05-19 13:05:28 -0700

    updated propTypes in characterCard

--- src/components/characters/CharacterCard.jsx
+++ src/components/characters/CharacterCard.jsx
@@ -15,25 +15,30 @@ const CharacterCard = ({
   return (
     <>
       <p>
-        <span>{name}</span>
+        <span>Name: {name}</span>
+        <span>Race: {race}</span>
+        <span>Spouse: {spouse}</span>
+        <span>Gender: {gender}</span>
+        <span>Hair Color: {hair}</span>
+        <span>Realm: {realm}</span>
+        <span>Birth: {birth}</span>
+        <span>Deceased: {death}</span>
+        <span>Learn More: {url}</span>
       </p>
     </>
   );
 };
 
 CharacterCard.propTypes = {
-  character: PropTypes.shape({
-    id: PropTypes.string.isRequired,
-    name: PropTypes.string.isRequired,
-    race: PropTypes.string.isRequired,
-    spouse: PropTypes.string.isRequired,
-    gender: PropTypes.string.isRequired,
-    hair: PropTypes.string.isRequired,
-    realm: PropTypes.string.isRequired,
-    birth: PropTypes.string.isRequired,
-    death: PropTypes.string.isRequired,
-    url: PropTypes.string.isRequired,
-  }),
+  birth: PropTypes.string.isRequired,
+  death: PropTypes.string.isRequired,
+  gender: PropTypes.string.isRequired,
+  hair: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  race: PropTypes.string.isRequired,
+  realm: PropTypes.string.isRequired,
+  spouse: PropTypes.string.isRequired,
+  url: PropTypes.string.isRequired,
 };
 
 export default CharacterCard;
